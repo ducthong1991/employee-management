b4f7233677a20cf91f403e4cdac518aab8a445be
Thong Hoang <hoangduc.thong@cartrack.com>
2025-04-04 11:28:50 +0800

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+const queryClient = new QueryClient();
+const theme = createTheme();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Routes>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
